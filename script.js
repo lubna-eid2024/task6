f709d00f0b24c951fdd3f741923c9ad34666383b
lubna-eid2024 <samynarmen7@gmail.com>
2024-12-13 14:01:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var num1 = +prompt("enter frist number")
+var num2 = +prompt("enter end number")
+var equel=false
+sum = num1 + num2;
+sub = num1 - num2;
+x = num1 / num2
+y = num1 % num2;
+z = num1 ** num2;
+a = num1 * num2;
+equel = num1 > num2;
+
+
+document.writeln(" <h1>The sum of plus : ", sum ,"</h1>")
+document.writeln("<h1>The sum of sub: ", sub, "</h1>");
+document.writeln("<h1>The sum of  divide : ", x), "</h1>";
+document.writeln(" <h1>The sum of modulus : ", y, "</h1>");
+document.writeln("<h1>The sum of power :" , z , "</h1>");
+document.writeln(" <h1>The sum of : ", equel , "</h1>");
+document.writeln(" <h1>The sum of  Multiplication:", a, "</h1> ");
+
+
+
+
+
+
+
+
+ 
